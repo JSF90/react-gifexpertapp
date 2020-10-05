d8994856dcb5394f4d4c43c124f186972095e6bb
Jorge <jorge90serra@gmail.com>
2020-10-04 21:22:07 -0400

    docs agregado

---
+++ docs/precache-manifest.a860ee2aaeb599c23660a6dc44b7ca17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9018beb1f50a447f708ade8f70ada88d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe11a104c9aa95d743c",
+    "url": "/static/css/main.038ae43e.chunk.css"
+  },
+  {
+    "revision": "080c2afeafe12f9f07ad",
+    "url": "/static/js/2.fc4b439f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc4b439f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe11a104c9aa95d743c",
+    "url": "/static/js/main.1c805326.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
